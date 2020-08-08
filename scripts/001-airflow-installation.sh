ee2cfd4d9c86c55ae81f1ee7a877eda0f35075b8
Worasit D <worasit.dmk501@gmail.com>
2020-08-08 08:54:45 +0700

    Added apache airflow installation script.

---
+++ scripts/001-airflow-installation.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# airflow needs a home, ~/airflow is the default,
+# but you can lay foundation somewhere else if you prefer
+# (optional)
+export AIRFLOW_HOME=~/airflow
+
+# install from pypi using pip
+pip install apache-airflow
+
+# initialize the database
+airflow initdb
+
+# start the web server, default port if 8080
+airflow webserver -p 8080
+
+# start the scheduler
+airflow scheduler
+
+# visit localhost:8080 in the browser and enable the example dag
+# in the home page
